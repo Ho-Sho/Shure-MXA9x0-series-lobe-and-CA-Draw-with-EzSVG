aa9926d87ea8e42524c81bde1e5e6d3f9a32b173
Ho-Sho <98727088+Ho-Sho@users.noreply.github.com>
2024-04-03 17:38:27 +0900

    version up 1.1.0 add option and active state

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Shure~Shure MXA9x0 Lobe or CA Draw with EzSVG",
-  Version = "1.0",
-  BuildVersion = "1.0.1.0",
+  Version = "1.1.0",
+  BuildVersion = "1.1.0.0",
   Id = "ad6824bd-42fd-4f49-ae32-c9338dc5d9e4",
   Author = "Ho",
   Description = "Shure MXA9x0 Series Lobe or CA Draw with EzSVG plugin"
